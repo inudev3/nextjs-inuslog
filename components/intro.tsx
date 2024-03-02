37b649c070ee5549aaa1fc63681eb84fc452af39
inujung <inujung@momenti.tv>
2024-03-03 00:25:51 +0900

    init

---
+++ components/intro.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { MotionValue, motion } from "framer-motion";
+import Image from "next/image";
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex  items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src="https://images.unsplash.com/photo-1709037813912-f2b0cfa924e4?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+              alt="image"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white objet-cover shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+    </section>
+  );
+}
